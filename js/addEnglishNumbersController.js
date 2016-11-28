b7308d3023d837d59f1c99691db122ea24e0be40
Yesid Daza <yiye_91@hotmail.com>
2016-11-27 21:33:17 -0500

    Get words without commas and hyphens from input field

--- js/addEnglishNumbersController.js
+++ js/addEnglishNumbersController.js
@@ -4,6 +4,20 @@ window.onload = function() {
        var firstNumber = document.getElementById('firstNumber').value;
        var secondNumber = document.getElementById('secondNumber').value;
        var answerLabel = document.getElementById('sumResult');
-       answerLabel.innerText = firstNumber;
+       answerLabel.innerText = convertTextToNumber(firstNumber);
     });
 };
+
+function convertTextToNumber(text) {
+    var words = "";
+    textArray = text.toLowerCase().toString().split(/[\s-,]+/);
+    for (var textItem = 0; textItem < textArray.length; textItem++) {
+        if (IsExists(textArray[textItem]))
+            words += (textArray[textItem]) + " ";
+    }
+    return words
+}
+
+function IsExists(variable) {
+    return !((variable == null) || (variable == ""));
+}
